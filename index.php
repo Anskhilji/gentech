c50d837f4598eda6b9a0fa0369dd80e64086c98c
Anskhilji <anskhilji900@gmail.com>
2020-11-21 17:09:09 +0500

    gentech

---
+++ index.php
@@ -0,0 +1,255 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <!-- Custom Styling -->
+  <link rel="stylesheet" href="css/styles.css">
+
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+  <title>Gentech</title>
+</head>
+
+<body>
+
+  <header class="header-bg">
+    <div class="header-wrapper">
+      <nav class="container">
+        <ul class="row">
+          <div class="col-md-8 col-sm-5 header-logo">
+            <li><a href="#"><img src="images/logo_b.png" class="logo"></a></li>
+          </div>
+          <div class="col-md-4 col-sm-7 header-links">
+            <div class="header-links-wrapper ml-auto">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#contact">Contact</a></li>
+              <li><a href="#work">Work</a></li>
+            </div>
+          </div>
+        </ul>
+      </nav>
+
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-8">
+            <h1 class="display-4 text-uppercase header-heading text-white">
+              GENTECH SOCIAL MEDIA<br>
+              MARKETING<br>
+              SERVICES</h1>
+            <p class=" mt-4 mb-4 text-white">Let us showcase your business and bring in new clients and customers</p>
+            <p class=" mt-4 text-white"><b>GET A FREE QUOTE</b> - Just leave your e-mail below and we will get in touch
+            </p>
+
+            <form class="row">
+              <div class="col-md-7  mt-2">
+                <input type="email" class="form-control" placeholder="*Enter Your Email Address">
+              </div>
+              <div class="col-md-5 mt-2">
+                <button type="submit" class=" form-control btn btn-danger text-white">Free Request Trial</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </header>
+
+  <section class="container" id="work">
+    <div class="row">
+      <div class="col-md-12 my-4">
+        <h2 class="my-4">OUR SUCCESS STORIES</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card w-100 my-3">
+          <img class="card-img-top" src="images/1.jpg" alt="Card image">
+          <div class="card-body">
+            <blockquote class="blockquote">
+              <p class="mb-0">"They understood the technical needs and what best practice looks like."</p>
+              <footer class="blockquote-footer">Itchy Palms Retailer</footer>
+            </blockquote>
+            <a href="#" class="">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card w-100 my-3">
+          <img class="card-img-top" src="images/2.jpg" alt="Card image">
+          <div class="card-body">
+            <blockquote class="blockquote">
+              <p class="mb-0">The final product helped garner a substantial increase in overall site traffic and
+                generated strong industry interest for the company. As a team, Convertiv was collaborative and
+                informative. Their deep knowledge and determination within the project made for a smooth engagement.</p>
+              <footer class="blockquote-footer">Goliath Engineering </footer>
+            </blockquote>
+            <a href="#" class="">Learn More</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card w-100 my-3">
+          <img class="card-img-top" src="images/3.jpg" alt="Card image">
+          <div class="card-body">
+            <blockquote class="blockquote">
+              <p class="mb-0">“They lead the relationship, which makes it really enjoyable to work with them.”</p>
+              <footer class="blockquote-footer">Veribium Dental Services</footer>
+            </blockquote>
+            <a href="#" class="">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <!-- carousel -->
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" style="height:400px" role="listbox">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active" style="height:400px;overflow:hidden;">
+          <div class="carousel-caption d-none d-md-block">
+            <p class="lead">"They quickly understood our organization and how to improve our online marketing.
+              I am a satisfied customer!"</p>
+            <h5>Andre Leboute</h5>
+            <h5>CEO, PetSupply NW</h5>
+          </div>
+          <img src="images/carousel-1.jpg" class="d-block w-100" alt="...">
+
+        </div>
+        <div class="carousel-item" style="height:400px;overflow:hidden;">
+          <div class="carousel-caption d-none d-md-block">
+            <p>"I especially like how they continue to revamp their reporting efforts for us.
+              They helped me succeed!"</p>
+            <h5>Michelle Lenbrook</h5>
+            <h5>Managing Director, Amazingly</h5>
+          </div>
+          <img src="images/carousel-2.jpg" class="d-block w-100" alt="...">
+
+        </div>
+        <div class="carousel-item" style="height:400px;overflow:hidden;">
+          <div class="carousel-caption d-none d-md-block">
+            <p>"Our sales have increased drastically since we first engaged Gentech … .
+              The results speak for themselves—the team listened to my needs and found the right approach."
+            </p>
+            <h5>Glenn Harrold</h5>
+            <h5>Marketing Manager</h5>
+
+          </div>
+          <img src="images/carousel-3.jpg" class="d-block w-100 img-fluid" alt="...">
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+  <div class="contact-form" id="contact">
+    <div class="container">
+      <h2>TELL US ABOUT YOUR REQUIREMENT</h2>
+
+      <div class="row">
+        <div class="col-md-6">
+          <?php
+          if(isset($_POST['username'])){
+              $to = "gentechint@zohomail.com";
+              
+              $subject = $_POST['username'];
+              $body = wordwrap($_POST['message'],70);
+              $header = "From: " .$_POST['email'];
+              
+              if( mail($to,$subject,$body,$header)){
+                  echo "<p class='text-success'>You message has been sent successfully!</p>";
+              
+              }else {
+                  echo "<p class='text-dnger'>Something Wrong!</p>";
+
+              }
+          }
+
+              
+          ?>
+         <form action="" method="post" class="mb-4">
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <input type="text" name="username" placeholder="*Your Name" class="form-control mb-2">
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" name="email" placeholder="*Your Email Address" class="form-control mb-2">
+
+                                </div>
+                            </div>
+
+                            <textarea class="form-control mb-2" name="message" placeholder="*Your Message" rows="3"></textarea>
+
+                        </div>
+
+                        <button type="submit" class="btn btn-danger">Submit</button>
+                    </form>
+        </div>
+
+        <div class="col-md-6 contact-align">
+          <div class="left">
+            <h5>Address</h5>
+            <p>Online United States</p>
+            <h5>Phone</h5>
+            <p>541-257-5171</p>
+            <h5>Email</h5>
+            <p>apexssmmg@gmail.com</p>
+          </div>
+        </div>
+      </div>
+
+
+
+    </div>
+  </div>
+  <footer class="footer-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-6">
+          <div>&copy; Gentech. All rights reserved.
+          </div>
+        </div>
+        <div class="col-6" style="text-align: right;">
+          <div>Design - Ans khan
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
+
+</body>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
